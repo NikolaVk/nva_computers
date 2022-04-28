8b6221b31cb4e656082a39c77bc66a22f1b3f5b2
NikolaVk <nikola@ique.nl>
2022-04-28 23:28:29 +0000

    Deploy 2nd step

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn nva-computers.wsgi:application
--- nva_computers/settings.py
+++ nva_computers/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1rf_uq)4&#p(fq%%r9f!+ueawuw9b0!2zhec=i9j75)_y)_vuq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nva-computers.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'nva_computers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
