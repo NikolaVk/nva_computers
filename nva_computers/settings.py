7dd9ab40c8df9a37fcc2b008307f0c4eee1f1075
NikolaVk <nikola@ique.nl>
2022-05-04 12:32:08 +0000

    Create new reviews app and make model

--- nva_computers/settings.py
+++ nva_computers/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['nva-computers.herokuapp.com', 'localhost']
 
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'reviews',
 
     # Other
     'crispy_forms',
---
+++ reviews/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from products.models import Product
+from django.contrib.auth.models import User
+from django.core.validators import (MaxValueValidator, MinValueValidator)
+
+# Create your models here.
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250, blank=True, null=True)
+    comment = models.TextField(max_length=250)
+    rating = models.IntegerField(default=5, validators=[MaxValueValidator(5),
+                MinValueValidator(1)])
+    created_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product.name
