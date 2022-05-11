9961e6172aecf21d02ed40ec365639c435908985
NikolaVk <nikola@ique.nl>
2022-05-11 11:02:29 +0000

    Create review.html, add reviews section to product detail and style it. Slight adjustment to model

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
+from django.db.models import Q, Avg
 from django.db.models.functions import Lower
 
 from reviews.models import Review
@@ -65,11 +65,13 @@ def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
-    review = Review.objects.filter(product_id=product_id)
+    reviews = Review.objects.filter(product_id=product_id)
+    score = Review.objects.filter(product_id=product_id).aggregate(Avg('rating'))
 
     context = {
         'product': product,
-        'review': review,
+        'reviews': reviews,
+        'score': score,
     }
 
     return render(request, 'products/product_detail.html', context)
--- reviews/models.py
+++ reviews/models.py
@@ -10,7 +10,7 @@ class Review(models.Model):
     user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     title = models.CharField(max_length=250, blank=True, null=True)
-    comment = models.TextField(max_length=250)
+    comment = models.TextField()
     rating = models.IntegerField(default=5, validators=[MaxValueValidator(5),
                                  MinValueValidator(1)])
     created_on = models.DateTimeField(auto_now=True)
