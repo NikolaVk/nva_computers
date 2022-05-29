477d73136ae9ed90fa45b8d263061b26b6559ce0
NikolaVk <nikola@ique.nl>
2022-05-29 23:13:51 +0000

    Create darkmode function and adjust all necesary css for it

--- static/js/base.js
+++ static/js/base.js
@@ -49,3 +49,54 @@
                         (navigator.maxTouchPoints > 0) ||
                         (navigator.msMaxTouchPoints > 0);
                 }
+
+                //DARKMODE
+
+                let allCookies = document.cookie.split(";").map((cookie) => cookie.trim());
+                let darkmode = 0;
+                if (allCookies && (allCookies.length > 0)) {
+                    for (let iIndex = 0; iIndex < allCookies.length; iIndex++) {
+                        let CookiePair = allCookies[iIndex].split("=");
+                        if ((CookiePair.length == 2) && (CookiePair[0] == 'darkmode')) {
+                            darkmode = parseInt(CookiePair[1]);
+                            if (darkmode == 1) {
+                                //                      window.addEventListener('load', function() {
+                                setdarkmode();
+                                //                      });
+                            }
+                        }
+                    }
+                }
+
+                function setlightmode() {
+                    let darkModeButton = document.getElementById("darkmode");
+                    let lightModeButton = document.getElementById("lightmode");
+
+                    darkModeButton.classList.add("darkmode_enabled");
+                    lightModeButton.classList.add("darkmode_disabled");
+                    darkModeButton.classList.remove("darkmode_disabled");
+                    lightModeButton.classList.remove("darkmode_enabled");
+
+                    let allElements = document.getElementsByTagName("*");
+                    for (let iIndex = 0; iIndex < allElements.length; iIndex++) {
+                        allElements[iIndex].classList.remove("godark");
+                    }
+                    document.cookie = "darkmode=0;path=/;expires=" + ((new Date()).getTime() + (365 * 24 * 3600 * 1000));
+                }
+
+                function setdarkmode() {
+                    let darkModeButton = document.getElementById("darkmode");
+                    let lightModeButton = document.getElementById("lightmode");
+
+                    darkModeButton.classList.add("darkmode_disabled");
+                    lightModeButton.classList.add("darkmode_enabled");
+                    darkModeButton.classList.remove("darkmode_enabled");
+                    lightModeButton.classList.remove("darkmode_disabled");
+
+                    let allElements = document.getElementsByTagName("*");
+                    for (let iIndex = 0; iIndex < allElements.length; iIndex++) {
+                        allElements[iIndex].classList.add("godark");
+                    }
+
+                    document.cookie = "darkmode=1;path=/;expires=" + ((new Date()).getTime() + (365 * 24 * 3600 * 1000));
+                }
