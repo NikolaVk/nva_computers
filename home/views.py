4e9aaa3910620a2f2f850a31e6f3941829bf0f3f
NikolaVk <nikola@ique.nl>
2022-06-03 10:36:12 +0000

    Create deals container shortcut on homepage

--- home/views.py
+++ home/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
-
-    return render(request, 'home/index.html')
+    x = Product.objects.all()
+    products = x.filter(category__name='deals')[:3]
+    context = {
+        'products': products,
+    }
+    return render(request, 'home/index.html', context)
