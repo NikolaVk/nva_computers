883a71e2d188fa636d2cf430ef8b4693b786c61b
NikolaVk <nikola@ique.nl>
2022-07-07 16:13:15 +0000

    Add send confirmation mail funtion to contact us

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.conf import settings
 from products.models import Product
 from .models import Contact
 from .forms import ContactForm
+from django.core.mail import send_mail
 
 
 def contact(request):
@@ -23,6 +28,7 @@ def contact(request):
             contact = contactform.save(commit=False)
 
             contact.save()
+            _send_confirmation_email(contact)
             messages.success(request, 'Succesfully submitted your message!')
             return redirect('/')
 
@@ -41,3 +47,23 @@ def contact(request):
         }
 
         return render(request, template, context)
+
+
+def _send_confirmation_email(contact):
+    """Send the user a confirmation email"""
+    contus_email = contact.mail
+    subject = render_to_string(
+        'contact/confirmation_emails/contactus_email_subject.txt',
+        {'contact': contact})
+    body = render_to_string(
+        'contact/confirmation_emails/contactus_email_body.txt',
+        {'contact': contact, 'contactus_email': settings.DEFAULT_FROM_EMAIL})
+
+    send_mail(
+        subject,
+        body,
+        settings.DEFAULT_FROM_EMAIL,
+        [contus_email]
+    )
+
+    return HttpResponse()
